d5f935c83fe7d982db9d69c52aa31c54962ea904
Miller <millerwjr@gmail.com>
2017-08-11 15:41:21 -0600

    clustergen v0.2
    Base functionality works - needs wrappers for file intake and random
    centroid generation.

---
+++ cmake-build-debug/CMakeFiles/clustergen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clustergen.dir/main.cpp.obj"
+  "CMakeFiles/clustergen.dir/clustergen.cpp.obj"
+  "clustergen.pdb"
+  "clustergen.exe"
+  "clustergen.exe.manifest"
+  "libclustergen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clustergen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
